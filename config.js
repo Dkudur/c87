47d321fbadac6776e017b6a959845e3886254a35
Dkudur <97417506+Dkudur@users.noreply.github.com>
2023-02-17 20:28:13 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8xrNWwWPnd8vVdzHwT7dowPY9bs2hEd4",
+  authDomain: "storytelling-ed273.firebaseapp.com",
+  databaseURL: "https://storytelling-ed273-default-rtdb.firebaseio.com",
+  projectId: "storytelling-ed273",
+  storageBucket: "storytelling-ed273.appspot.com",
+  messagingSenderId: "275295610398",
+  appId: "1:275295610398:web:01e6a6696f468eea81700f"
+};
---
+++ screens/Feed.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import StoryCard from "./StoryCard";
+
+import * as Font from "expo-font";
+import { FlatList } from "react-native-gesture-handler";
+
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+let stories = require("./temp_stories.json");
+
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      stories: [],
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchStories();
+  }
+
+  fetchStories = () => {
+    firebase
+      .database()
+      .ref("/posts/")
+      .on("value", (info) => {
+        var stories = [];
+        if (info.val()) {
+          Object.keys(info.val()).forEach(function () {
+            stories.push({
+              key: key,
+              value: info.val()[key],
+            });
+          });
+        }
+
+        this.setState({ stories: stories });
+      });
+  };
+
+  renderItem = ({ item: story }) => {
+    return <StoryCard story={story} navigation={this.props.navigation} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Storytelling App</Text>
+            </View>
+          </View>
+
+          {!this.state.stories[0] ? (
+            <View>
+              <Text  style={this.state.light_theme  ? styles.noStoriesTextLight : styles.noStoriesText }>
+                No stories available
+              </Text>
+            </View>
+          ) 
+          : 
+          (
+            <View style={styles.cardContainer}>
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={stories}
+                renderItem={this.renderItem}
+              />
+            </View>
+          )}
+
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  cardContainer: {
+    flex: 0.85,
+  },
+  noStories: {
+    flex: 0.85,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  noStoriesTextLight: {
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  noStoriesText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  }
+});
